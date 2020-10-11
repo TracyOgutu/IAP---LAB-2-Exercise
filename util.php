c9df6c14439528eef4f3bb2a78b47e04c2123e6e
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-11 21:29:58 +0300

    add static variables for db connection

---
+++ util.php
@@ -0,0 +1,10 @@
+<?php
+class Util {			
+    //About DB
+    static $DB_NAME = "loginphp";
+    static $DB_USER = "tracy";
+    static $DB_USER_PASS = "Wakanda2030";
+    static $SERVER_NAME = "localhost";
+}
+
+?>
