76596aa32676736e569015edfbf338e678cfb004
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-11 21:29:24 +0300

    add homepage UI

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Dashboard</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body class="home">
+    <div class="container">
+        <div class="row">
+         <div class="col-lg-9">
+            <h2 class="card-title text-center">Welcome Home</h2><br>
+         </div><br>       
+<div class="col-lg-3">
+    <button class="btn btn-lg btn-primary btn-block text-uppercase" id ="logoutbutton" type="submit">
+        <a href="http://localhost/phplogin/login.php">Log out</a>
+    </button>
+</div>
+            
+           
+          </div>
+        </div>
+      </div>
+</body>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+</html>
