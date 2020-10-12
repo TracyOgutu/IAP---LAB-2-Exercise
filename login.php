a96b0890030c1d159b46dfb8137d667694486399
Tracy Ogutu <tracyogutu@gmail.com>
2020-10-12 13:01:52 +0300

    change log in requirements

--- login.php
+++ login.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="index.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User Login</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-          <div class="col-lg-10 col-xl-9 mx-auto">
-            <div class="card card-signin flex-row my-5">
-              <div class="card-img-left d-none d-md-flex">
-                 <!-- Background image for card set in CSS! -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card card-signin flex-row my-5">
+          <div class="card-img-left d-none d-md-flex">
+            <!-- Background image for card set in CSS! -->
+          </div>
+          <div class="card-body">
+            <h5 class="card-title text-center">Log in</h5>
+            <form class="form-signin" action="index.php" method="POST">
+              <div class="form-label-group">
+                <input type="text" id="myusername" name="username" class="form-control" placeholder="Username" required>
+                <label for="myusername">Username</label>
               </div>
-              <div class="card-body">
-                <h5 class="card-title text-center">Log in</h5>
-                <form class="form-signin">
-                  
-                  <div class="form-label-group">
-                    <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required>
-                    <label for="email">Email address</label>
-                  </div>
-                  <hr>
-                  <div class="form-label-group">
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                    <label for="inputPassword">Password</label>
-                  </div>            
-                  <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit"><a href="http://localhost/phplogin/home.php">Log in</a></button>
-                  <a class="d-block text-center mt-2 small" href="http://localhost/phplogin/register.php">Sign Up</a>
-                  <a class="d-block text-center mt-2 small" href="http://localhost/phplogin/changepassword.php">Change password</a>
-                  <hr class="my-4">
-                  <!-- <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
+              <hr>
+              <div class="form-label-group">
+                <input type="password" id="mypassword" name="password" class="form-control" placeholder="Password" required>
+                <label for="mypassword">Password</label>
+              </div>
+
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="login">Log in</button>
+              <a class="d-block text-center mt-2 small" href="http://localhost/phplogin/register.php">Sign Up</a>
+              <a class="d-block text-center mt-2 small" href="http://localhost/phplogin/changepassword.php">Change password</a>
+              <hr class="my-4">
+              <!-- <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
                   <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button> -->
-                </form>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
+    </div>
+  </div>
 </body>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+
 </html>
